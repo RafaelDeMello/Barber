20c53431f1353d210273c836b5f97b83c3989365
Rafael de Mello <rafaeldemello2023@gmail.com>
2025-08-18 17:48:27 -0300

    Feat: add Header components and banner

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,36 @@
+import { SearchIcon } from "lucide-react";
 import { Button } from "./_components/ui/button";
+import Header from "./_components/ui/header";
+import { Input } from "./_components/ui/input";
+import Image from "next/image";
 
 const Home = () => {
-  return <Button>teste</Button>
-}
+  return (
+  <div>
 
+    <Header />
+    <div className="p-5">
+      <h2 className="text-xl font-bold">Olá, Rafael!</h2>
+      <p>Segunda feira, 18 de agosto. </p>
+
+      <div className="mt-6 flex items-center gap-2">
+        <Input placeholder="Faça sua reserva" />
+        <Button>
+          <SearchIcon />
+        </Button>
+      </div>
+
+      <div className="relative w-full h-[150px] mt-6 ">
+        <Image
+         alt="Agende nos melhores com FSW Barber"
+         src="/banner-01.png" 
+         fill 
+         className="object-cover rounded-xl"/>
+      </div>
+    </div>
+
+    
+  </div>
+  )
+}
 export default Home;
