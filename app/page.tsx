f95c25c493924f56e8a0c7fa6f10480218ae93d2
Rafael de Mello <rafaeldemello2023@gmail.com>
2025-08-18 18:11:48 -0300

    Feat: add Booking iten

--- app/page.tsx
+++ app/page.tsx
@@ -1,36 +1,65 @@
-import { SearchIcon } from "lucide-react";
-import { Button } from "./_components/ui/button";
-import Header from "./_components/ui/header";
-import { Input } from "./_components/ui/input";
-import Image from "next/image";
+import { SearchIcon } from "lucide-react"
+import { Button } from "./_components/ui/button"
+import Header from "./_components/ui/header"
+import { Input } from "./_components/ui/input"
+import Image from "next/image"
+import { CardContent } from "./_components/ui/card"
+import { Card } from "./_components/ui/card"
+import { Badge } from "./_components/ui/badge"
+import { Avatar, AvatarImage, AvatarFallback } from "./_components/ui/avatar"
 
 const Home = () => {
   return (
-  <div>
+    <div>
+      {/* Header */}
+      <Header />
+      <div className="p-5">
+        {/* Texto */}
+        <h2 className="text-xl font-bold">Olá, Rafael!</h2>
+        <p>Segunda feira, 18 de agosto. </p>
 
-    <Header />
-    <div className="p-5">
-      <h2 className="text-xl font-bold">Olá, Rafael!</h2>
-      <p>Segunda feira, 18 de agosto. </p>
+        {/* Busca */}
+        <div className="mt-6 flex items-center gap-2">
+          <Input placeholder="Faça sua reserva" />
+          <Button>
+            <SearchIcon />
+          </Button>
+        </div>
 
-      <div className="mt-6 flex items-center gap-2">
-        <Input placeholder="Faça sua reserva" />
-        <Button>
-          <SearchIcon />
-        </Button>
-      </div>
+        {/* Imagem */}
+        <div className="relative mt-6 h-[150px] w-full">
+          <Image
+            alt="Agende nos melhores com FSW Barber"
+            src="/banner-01.png"
+            fill
+            className="rounded-xl object-cover"
+          />
+        </div>
 
-      <div className="relative w-full h-[150px] mt-6 ">
-        <Image
-         alt="Agende nos melhores com FSW Barber"
-         src="/banner-01.png" 
-         fill 
-         className="object-cover rounded-xl"/>
+        {/* Agendamentos */}
+        <Card className="mt-6">
+          <CardContent className="flex justify-between p-0">
+            {/* Esquerda */}
+            <div className="flex flex-col gap-2 py-5 pl-5">
+              <Badge className="w-fit">Confirmado</Badge>
+              <h3 className="font-semibold">Corte de Cabelo</h3>
+              <div className="flex items-center gap-2">
+                 <Avatar className="h-6 w-6">
+                  <AvatarImage src="https://utfs.io/f/c97a2dc9-cf62-468b-a851-bfd2bdde775f-16p.png"/>
+                 </Avatar>
+                 <p className="text-sm">Barbearia FSW</p>
+              </div>
+            </div>
+            {/* Direita */}
+            <div className="flex flex-col items-center justify-center px-5 border-l-2 border-solid">
+              <p className="text-sm">Agosto</p>
+              <p className="text-2xl">23</p>
+              <p className="text-sm">20:00</p>
+            </div>
+          </CardContent>
+        </Card>
       </div>
     </div>
-
-    
-  </div>
   )
 }
-export default Home;
+export default Home
