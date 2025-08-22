9cb118a0542d1461deca8ee7db1ad5c0788c3e9f
Rafael de Mello <rafaeldemello2023@gmail.com>
2025-08-21 23:59:48 -0300

    Update search.tsx

--- app/_components/search.tsx
+++ app/_components/search.tsx
@@ -1,30 +1,58 @@
 "use client"
 
-import { Input } from "./ui/input"
-import { Button } from "./ui/button"
 import { SearchIcon } from "lucide-react"
-import { useState } from "react"
+import { Button } from "./ui/button"
+import { Input } from "./ui/input"
 import { useRouter } from "next/navigation"
+import { zodResolver } from "@hookform/resolvers/zod"
+
+import { z } from "zod"
+import { useForm } from "react-hook-form"
+import { Form, FormControl, FormField, FormItem, FormMessage } from "./ui/form"
+
+const formSchema = z.object({
+  title: z.string().trim().min(1, {
+    message: "Digite algo para buscar",
+  }),
+})
 
 const Search = () => {
-  const [search, setSearch] = useState("")
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      title: "",
+    },
+  })
   const router = useRouter()
 
-  const handleSubmit = (e) => {
-    e.preventDefault()
-    router.push(`/barbershops?search=${search}`)
+  const handleSubmit = (data: z.infer<typeof formSchema>) => {
+    router.push(`/barbershops?title=${data.title}`)
   }
+
   return (
-    <form onSubmit={handleSubmit} className="flex items-center gap-2">
-      <Input
-        placeholder="Faça sua reserva"
-        value={search}
-        onChange={(e) => setSearch(e.target.value)}
-      />
-      <Button type="submit">
-        <SearchIcon />
-      </Button>
-    </form>
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(handleSubmit)} className="flex gap-2">
+        <FormField
+          control={form.control}
+          name="title"
+          render={({ field }) => (
+            <FormItem className="w-full">
+              <FormControl>
+                <Input
+                  placeholder="Faça sua busca..."
+                  {...field}
+                  className="w-full"
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">
+          <SearchIcon />
+        </Button>
+      </form>
+    </Form>
   )
 }
 
